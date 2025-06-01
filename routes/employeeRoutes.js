47c97e1a9bcbab153be2efe87ca099ce3384f8c7
raonedev <kumaraman33063@gmail.com>
2025-06-01 23:07:00 +0530

    adding middleware in admin protal

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const employeeController = require('../controllers/employeeController'); // Import the employee controller
 
 // Define routes and link them to controller functions for employees
-router.post('/', employeeController.createEmployee); // Route for creating a new employee
-router.get('/', employeeController.getAllEmployees); // Route for getting all employees
-router.get('/:id', employeeController.getEmployeeById); // Route for getting a single employee by ID
-router.put('/:id', employeeController.updateEmployee); // Route for updating an employee by ID
-router.delete('/:id', employeeController.deleteEmployee); // Route for deleting an employee by ID
+router.post('/',protect, authorize('admin'), employeeController.createEmployee); // Route for creating a new employee
+router.get('/',protect, authorize('admin'), employeeController.getAllEmployees); // Route for getting all employees
+router.get('/:id',protect, authorize('admin'), employeeController.getEmployeeById); // Route for getting a single employee by ID
+router.put('/:id',protect, authorize('admin'), employeeController.updateEmployee); // Route for updating an employee by ID
+router.delete('/:id',protect, authorize('admin'), employeeController.deleteEmployee); // Route for deleting an employee by ID
 
 module.exports = router; // Export the router
--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -2,12 +2,13 @@
 const express = require('express');
 const router = express.Router(); // Create an Express Router instance
 const roomController = require('../controllers/roomController'); // Import the room controller
+const { protect, authorize } = require('../middleware/authMiddleware');
 
 // Define routes and link them to controller functions
-router.post('/', roomController.createRoom); // Route for creating a new room
-router.get('/', roomController.getAllRooms); // Route for getting all rooms
-router.get('/:id', roomController.getRoomById); // Route for getting a single room by ID
-router.put('/:id', roomController.updateRoom); // Route for updating a room by ID
-router.delete('/:id', roomController.deleteRoom); // Route for deleting a room by ID
+router.post('/',protect, authorize('admin'), roomController.createRoom); // Route for creating a new room
+router.get('/',protect, roomController.getAllRooms); // Route for getting all rooms
+router.get('/:id',protect, roomController.getRoomById); // Route for getting a single room by ID
+router.put('/:id',protect, authorize('admin'), roomController.updateRoom); // Route for updating a room by ID
+router.delete('/:id',protect, authorize('admin'), roomController.deleteRoom); // Route for deleting a room by ID
 
 module.exports = router; // Export the router
