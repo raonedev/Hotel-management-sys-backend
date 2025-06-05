0bae133c1d4342cbcc5ac5507627db3079df404f
raonedev <kumaraman33063@gmail.com>
2025-06-05 09:20:37 +0530

    and env file

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -5,7 +5,7 @@ const bookingController = require('../controllers/bookingController'); // Import
 const { protect, authorize } = require('../middleware/authMiddleware');
 
 // Define routes and link them to controller functions for bookings
-router.post('/', bookingController.createBooking);
+router.post('/',protect, bookingController.createBooking);
 router.get('/',protect, authorize('admin'), bookingController.getAllBookings);
 router.get('/:id',protect, bookingController.getBookingById);
 router.put('/:id',protect, bookingController.updateBooking);
